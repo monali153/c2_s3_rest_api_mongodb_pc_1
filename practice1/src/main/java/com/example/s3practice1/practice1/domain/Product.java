3c7cc014d4f22132b7a7e1859394c103e35452bb
Monali Sharma <mona.chaudhari444@gmail.com>
2022-11-07 18:06:49 +0530

    pc complete

---
+++ practice1/src/main/java/com/example/s3practice1/practice1/domain/Product.java
@@ -0,0 +1,44 @@
+package com.example.s3practice1.practice1.domain;
+
+public class Product {
+
+    private int productId;
+    private String productName;
+    private String productDescription;
+
+    public Product() {
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                '}';
+    }
+}
